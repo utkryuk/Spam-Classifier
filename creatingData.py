7289865c1018d858cb76008f9aafbd9f3dc5b15b
Utkarsh Gupta <utkryuk@gmail.com>
2019-08-12 19:38:50 +0530

    Adding files

---
+++ creatingData.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 12 17:03:52 2019
+
+@author: Ezone
+"""
+
+import numpy as np
+import pandas as pd
+
+ham = pd.read_csv('ham.csv')
+spam = pd.read_csv('spam.csv')
+
+
+Class =[]
+Content = []
+for i in range(ham.shape[0]):
+    Class.append('ham')
+    Content.append(ham['Message'][i])
+    
+for i in range(spam.shape[0]):
+    Class.append('spam')
+    Content.append(spam['Message'][i])
+
+
+data = {'Message': Content, 'Class': Class}
+data = pd.DataFrame(data)
+
+data = data.iloc[np.random.permutation(len(data))] #mixing spam and ham messages
+
+#splitting into train and test data
+from sklearn.model_selection import train_test_split
+train, test = train_test_split(data,test_size= 0.2,random_state =42)
+
+
+prediction_ans = test['Class']
+prediction_ans = {'Class': prediction_ans}
+prediction_ans = pd.DataFrame(prediction_ans)
+
+del test['Class']
+
+#creating train, test
+train.to_csv('train.csv',index = False)
+test.to_csv('test.csv',index = False)
+prediction_ans.to_csv('prediction_ans.csv',index = False)
---
+++ spam_classifier.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  5 02:56:37 2019
+
+@author: Ezone
+"""
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import pandas as pd
+
+train = pd.read_csv('train.csv')
+
+vectoriser = CountVectorizer()
+count = vectoriser.fit_transform(train['Message'].values)
+
+
+target = train['Class'].values
+
+
+classifier = MultinomialNB()
+classifier.fit(count, target)
+
+
+test = pd.read_csv('test.csv')
+prediction = vectoriser.transform(test['Message'])
+
+
+predictionfromNaiveBayes = classifier.predict(prediction)
+
+prediction_ans = pd.read_csv('prediction_ans.csv')
+predictionfromNaiveBayes = {'Class': predictionfromNaiveBayes}
+predictionfromNaiveBayes = pd.DataFrame(predictionfromNaiveBayes)
+
+#for checking accuracy
+right = 0
+for i in range(prediction_ans.shape[0]):
+    if str(prediction_ans['Class'][i]) == str(predictionfromNaiveBayes['Class'][i]):
+        right = right+1
+        
+#accuracy
+accuracy = right/prediction.shape[0]
