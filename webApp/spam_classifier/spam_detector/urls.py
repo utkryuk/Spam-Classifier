8b6a20c6ea4a41cda145f987366382ce78b5dc76
utkryuk <utkryuk@gmail.com>
2020-08-08 01:27:49 +0530

    Edited and changed filenames

--- webApp/spam_classifier/spam_detector/urls.py
+++ webApp/spam_classifier/spam_detector/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('predict/', views.predict, name='success'),
-    path('', views.indexPage, name='index')
+    path('predict/', views.predict, name='result'),
+    path('', views.indexPage, name='classifier')
 ]
--- webApp/spam_classifier/spam_detector/views.py
+++ webApp/spam_classifier/spam_detector/views.py
@@ -14,30 +14,14 @@ cv = pickle.load(open(os.path.join(ML_MODEL_DIR, 'count-transform.pkl'),'rb'))
 classifier = pickle.load(open(os.path.join(ML_MODEL_DIR, 'spam-or-ham-classifier.pkl'),'rb'))
 
 def indexPage(request):
-    return render(request, 'index.html')
+    return render(request, 'classifier.html')
 
 def predict(request):
     if request.method == 'POST':
         emailBody = request.POST['email_body']
-        # emailBody = request.form['email_body']
         data = [emailBody]
         vect = cv.transform(data).toarray()
         my_prediction = classifier.predict(vect)
-        # return render(request ,'success.html',prediction=my_prediction)
-        # return render(request, 'fail.html')
-        print(my_prediction[0])
-        print(type(my_prediction))
-        return render(request, 'success.html', {"prediction": my_prediction[0]})
+        return render(request, 'result.html', {"prediction": my_prediction[0]})
     else:
         return render(request, 'fail.html')
-    # emailBody = request.POST.get("email_body")
-    # print(emailBody)
-    # data = [Email_body]
-    # print(data)
-    # email_series = pd.Series(emailBody)
-    # vect = cv.transform(email_series)
-    # my_prediction = classifier.predict(vect)
-    # return render(request, 'success.html', prediction=my_prediction)
-    # return render(request, 'success.html', {"prediction": my_prediction})
-
-    # return HttpResponse('<h1>Page was found</h1>')
