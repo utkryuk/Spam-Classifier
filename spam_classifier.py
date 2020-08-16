86eec83c40f5ebde4af646fe2ccf2c5211f34510
utkryuk <utkryuk@gmail.com>
2020-08-17 03:56:12 +0530

    Final Changes.

--- spam_classifier.py
+++ spam_classifier.py
@@ -4,63 +4,66 @@ from sklearn.naive_bayes import MultinomialNB
 import pandas as pd
 import pickle
 
-train = pd.read_csv('train.csv')
+class SpamClassifier:
 
-vectoriser = CountVectorizer()
-count = vectoriser.fit_transform(train['Message'].values)
+    def saveModel(self):
+        train = pd.read_csv('train.csv')
+        vectoriser = CountVectorizer()
+        count = vectoriser.fit_transform(train['Message'].values)
 
-# print(count)
+        # print(count)
 
-pickle.dump(vectoriser, open('count-transform.pkl', 'wb'))
+        pickle.dump(vectoriser, open('count-transform.pkl', 'wb'))
 
-target = train['Class'].values
+        target = train['Class'].values
 
-# Model Build
+        # Model Build
 
-classifier = MultinomialNB()
-classifier.fit(count, target)
+        classifier = MultinomialNB()
+        classifier.fit(count, target)
 
-# Creating the pickle file for Naive Bayes Model
+        # Creating the pickle file for Naive Bayes Model
 
-filename = 'spam-or-ham-classifier.pkl'
-pickle.dump(classifier, open(filename, 'wb'))
+        filename = 'spam-or-ham-classifier.pkl'
+        pickle.dump(classifier, open(filename, 'wb'))
 
-test = pd.read_csv('test.csv')
+        print("Models are saved. Bye...\U0001F44B")
+
+    def checkAccuracy(self):
+        train = pd.read_csv('train.csv')
+        vectoriser = CountVectorizer()
+        count = vectoriser.fit_transform(train['Message'].values)
+
+        target = train['Class'].values
+
+        classifier = MultinomialNB()
+        classifier.fit(count, target)
+
+        test = pd.read_csv('test.csv')
 
 
-prediction = vectoriser.transform(test['Message'])
+        prediction = vectoriser.transform(test['Message'])
 
-'''
-################################################################
-print(prediction.shape)
-print(test['Message'].shape)
-# print(vectoriser.transform(test['Message'][0]))
-# print(prediction)
-print(type(test['Message']))
+        predictionfromNaiveBayes = classifier.predict(prediction)
 
-abc = test['Message'][0]
-abc_series = pd.Series(abc)
-print(abc_series.shape)
-# print(prediction)
-print(vectoriser.transform(abc_series))
-print(type(vectoriser.transform(abc_series)))
+        prediction_ans = pd.read_csv('prediction_ans.csv')
+        predictionfromNaiveBayes = {'Class': predictionfromNaiveBayes}
+        predictionfromNaiveBayes = pd.DataFrame(predictionfromNaiveBayes)
 
-################################################################
-'''
+        #for checking accuracy
+        right = 0
+        for i in range(prediction_ans.shape[0]):
+            if str(prediction_ans['Class'][i]) == str(predictionfromNaiveBayes['Class'][i]):
+                right = right+1
+                
+        #accuracy
+        accuracy = right/prediction.shape[0]
 
-predictionfromNaiveBayes = classifier.predict(prediction)
+        print("Accuracy of the test set: ",accuracy*100, "%")
 
-prediction_ans = pd.read_csv('prediction_ans.csv')
-predictionfromNaiveBayes = {'Class': predictionfromNaiveBayes}
-predictionfromNaiveBayes = pd.DataFrame(predictionfromNaiveBayes)
+obj = SpamClassifier()
 
-#for checking accuracy
-right = 0
-for i in range(prediction_ans.shape[0]):
-    if str(prediction_ans['Class'][i]) == str(predictionfromNaiveBayes['Class'][i]):
-        right = right+1
-        
-#accuracy
-accuracy = right/prediction.shape[0]
+# To check the accuracy of the test set
+# obj.checkAccuracy()
 
-print(accuracy)
+obj.saveModel()
